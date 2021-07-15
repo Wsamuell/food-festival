d77815cb16065f308da8e875351bab90ed6a3a67
samuel Odubamowo <wemsamuel@gmail.com>
2021-07-15 13:49:51 -0700

    updated bootstrap and popper for 19.2.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
-    Plugins: [
+    plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
